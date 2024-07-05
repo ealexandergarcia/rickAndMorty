cbcefcf4b9b032b79125d67bc48a8be698e49649
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-07-05 12:47:15 -0500

    feat: :sparkles: Busqueda por nombre

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
 import { characterCard } from "./component/card.js";
 import { getAllCharacter } from "./module/requests.js";
 
+    
+// Agregamos un evento de submit al formulario de búsqueda
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const searchTerm = document.getElementById('search-input').value.trim();
+  const url = `https://rickandmortyapi.com/api/character/?name=${searchTerm}`;
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    const card = document.querySelector(".cards");
+    card.innerHTML = await characterCard(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// Cargamos la lista de personajes al inicio
 let data = await getAllCharacter();
-console.log(data);
-let card = document.querySelector(".cards")
-
+let card = document.querySelector(".cards");
 card.innerHTML = await characterCard(data);
-
-
