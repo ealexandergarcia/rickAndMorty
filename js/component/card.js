943eebae1148b9715afabf9445567d5d998538b6
ealexandergarcia <106351807+ealexandergarcia@users.noreply.github.com>
2024-07-05 12:47:50 -0500

    refactor: :recycle: REfactorizacion del componente de card

--- js/component/card.js
+++ js/component/card.js
@@ -1,39 +1,48 @@
 import { getOneEpisode } from "../module/requests.js";
 
 export const characterCard = async (res) => {
-    let { results } = res;
-
-    let plantilla = "";
-
-
-    for (const element of results) {
-        let firstEpisdoCharacter = element.episode[0];
-        let { location: {name :nameLocation} } = element
-        console.log(nameLocation);
-        let dataEpisode = await getOneEpisode(firstEpisdoCharacter);
-
-        plantilla +=  /*html*/`
+    const characters = res.results;
+    const cards = [];
+  
+    for (const character of characters) {
+      const { name, image, species, status, episode } = character;
+      const firstEpisode = episode[0];
+      const { name: locationName } = character.location;
+      const episodeData = await getOneEpisode(firstEpisode);
+  
+      const card = {
+        name,
+        image,
+        species,
+        status,
+        locationName,
+        firstEpisodeName: episodeData.name,
+      };
+  
+      const cardTemplate = `
         <div class="card">
-        <img src="${element.image}" class="card-image" alt="Abadango Cluster Princess">
-        <div>
+          <img src="${card.image}" class="card-image" alt="${card.name}">
+          <div>
             <div class="card-header">
-                <h2 class="card-title">${element.name}</h2>
-                <span>${element.species} - ${element.status}</span>
+              <h2 class="card-title">${card.name}</h2>
+              <span>${card.species} - ${card.status}</span>
             </div>
             <div class="card-info">
-                <div class="info-item">
-                    <span class="label">Last known location:</span>
-                    <span class="value">${nameLocation}</span>
-                </div>
-                <div class="info-item">
-                    <span class="label">First seen in:</span>
-                    <span class="value">${dataEpisode.name}</span>
-                </div>
+              <div class="info-item">
+                <span class="label">Last known location:</span>
+                <span class="value">${card.locationName}</span>
+              </div>
+              <div class="info-item">
+                <span class="label">First seen in:</span>
+                <span class="value">${card.firstEpisodeName}</span>
+              </div>
             </div>
+          </div>
         </div>
-    </div> `
-    };
-
-    return plantilla;
-
-}
+      `;
+  
+      cards.push(cardTemplate);
+    }
+  
+    return cards.join('');
+  };
